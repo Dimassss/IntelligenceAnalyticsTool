fdf825daacac9756be81723d77554001c106c73a
Dmytro Karpus <dimas.karpusss@gmail.com>
2022-09-05 23:11:15 +0300

    Initial commit for frontend

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+    build: {
+        babel: {
+          presets({isServer}) {
+                const targets = isServer ? { node: 'current' } : { ie: 11 }
+                return [
+                    [ require.resolve("@babel/preset-env"), { targets }  ]
+                ]
+            },
+            plugins: [
+                "@babel/syntax-dynamic-import",
+                "@babel/transform-runtime",
+                "@babel/transform-async-to-generator"
+            ]
+        },
+    }
+}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 v-for="item in strings" :key="item">
+            This is string "{{ item }}"
+        </h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            strings: Array.from("sdfasdf").map((el,i) => "str-" + i)
+        }
+    }
+}
+</script>
